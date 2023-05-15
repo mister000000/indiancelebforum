70414a13e664e12cd5c6ad15396f730121e2ec36
mister000000 <mister000000@protonmail.com>
2023-05-16 00:12:07 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta name="p:domain_verify" content="a71828890c61affd5ae020a4e2b919f3"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
